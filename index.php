321175af5278a77c2842fd01fec0cec018cfce14
marcusschonning <marcus.schonning@gmail.com>
2015-06-02 11:03:28 +0200

    small fixes
    fixings

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-
+<?php get_header(); ?>
 		<p>Hello world! This is HTML5 Boilerplate.</p>
-
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
-		<script src="js/plugins.js"></script>
-		<script src="js/main.js"></script>
-
-	</body>
-</html>
+<?php get_footer(); ?>
