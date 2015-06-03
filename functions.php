2c8e78efc9e9924821fd9562e1916f8074bcce15
marcusschonning <marcus.schonning@gmail.com>
2015-06-03 16:22:26 +0200

    aölkdfns

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  **********************************************/
 function hm_scripts() {
 	wp_enqueue_style('font-awesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css');
-	wp_enqueue_style('style', get_stylesheet_uri().'/build/css/main.css');
+	wp_enqueue_style('style', get_template_directory_uri() . '/build/css/main.css');
 
 	wp_enqueue_script('modernizr', get_template_directory_uri() . '/build/js/vendor/modernizr-2.8.3.min.js', array(), '', false);
 	wp_enqueue_script('jquery', '//code.jquery.com/jquery-1.11.2.min.js', array(), '', true);
