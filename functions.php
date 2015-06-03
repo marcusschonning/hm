4e152cfac28a0982eca50acd7be7ba9155783d06
Joakim Forslund <JoakimForslund@Joakims-MacBook-Pro.local>
2015-06-03 16:03:10 +0200

    Added a whole bunch of shit

--- functions.php
+++ functions.php
@@ -7,11 +7,11 @@
  **********************************************/
 function hm_scripts() {
 	wp_enqueue_style('font-awesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css');
-	wp_enqueue_style('style', get_stylesheet_uri());
+	wp_enqueue_style('style', get_stylesheet_uri().'/build/css/main.css');
 
-	wp_enqueue_script('modernizr', get_template_directory_uri() . '/js/vendor/modernizr-2.8.3.min.js', array(), '', false);
+	wp_enqueue_script('modernizr', get_template_directory_uri() . '/build/js/vendor/modernizr-2.8.3.min.js', array(), '', false);
 	wp_enqueue_script('jquery', '//code.jquery.com/jquery-1.11.2.min.js', array(), '', true);
-	wp_enqueue_script('script', get_template_directory_uri() . '/js/main.js', array(), '', true);
+	wp_enqueue_script('script', get_template_directory_uri() . '/build/js/main.min.js', array(), '', true);
 }
 add_action( 'wp_enqueue_scripts', 'hm_scripts' );
 
