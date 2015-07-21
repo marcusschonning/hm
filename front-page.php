c6033a8dcfe727734241d2367059b28be17798ae
Joakim Forslund <JoakimForslund@Joakims-MacBook-Pro.local>
2015-07-21 14:20:09 +0200

    added background video

--- front-page.php
+++ front-page.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
+<?php get_header(); ?>
 <body>
 <p>penis</p>
+<video class="bg-vid" autoplay loop>
+	<source src="<?php echo get_template_directory_uri();?>/img/test-bg-loop.mp4" />
+</video>
 
 </body>
-</html>
+<?php get_footer(); ?>
