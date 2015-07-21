fbe5cc4b80fe12e301c712edbf66b17e95963e8d
marcusschonning <marcus.schonning@gmail.com>
2015-07-21 15:19:38 +0200

    hej jocke

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,43 @@
 jQuery(function($){
 	var products = [];
 
-	if($('body').hasClass('post-type-archive-products')){
-		$('.product-link').on('click', function(e) {
-			e.preventDefault();
-			var url = $(this).attr('href');
-			$.ajax({
-				url: url
-			}).done(function(res) {
-				$('.content').html( res );
-			});
+	
+	$('.product-link').on('click', function(e) {
+		e.preventDefault();
+		var url = $(this).attr('href');
+		$.ajax({
+			url: url
+		}).done(function(res) {
+			$('.content').html( res );
 		});
+	});
 
-		$('.content').delegate('.close', 'click', function(){
-			$('.content').html('');
-		});
+	$('.content').delegate('.close', 'click', function(){
+		$('.content').html('');
+	});
 
-		$('.content').delegate('.add-to-cart', 'click', function(){
-			
-			var id = parseInt($(this).siblings('.title').attr('product-id'));
-			var price = parseInt($(this).siblings('.price').text());
-			var color = $(this).siblings('.colors').children('input[type="radio"][name="color"]:checked').val();
-			var size = $(this).siblings('.sizes').children('input[type="radio"][name="size"]:checked').val();
-			products.push({
-				'id': id,
-				'price': price,
-				'color': color,
-				'size': size
-			});
-			console.log(id);
-			console.log(price);
-			console.log(color);
-			console.log(size);
-			console.log(products);
-			
-			var obj = JSON.stringify({ 'products': products });
-			localStorage.setItem('products', obj);
+	$('.content').delegate('.add-to-cart', 'click', function(){
+		
+		var id = parseInt($(this).siblings('.title').attr('product-id'));
+		var price = parseInt($(this).siblings('.price').text());
+		var color = $(this).siblings('.colors').children('input[type="radio"][name="color"]:checked').val();
+		var size = $(this).siblings('.sizes').children('input[type="radio"][name="size"]:checked').val();
+		products.push({
+			'id': id,
+			'price': price,
+			'color': color,
+			'size': size
 		});
-	}
+		console.log(id);
+		console.log(price);
+		console.log(color);
+		console.log(size);
+		console.log(products);
+		
+		var obj = JSON.stringify({ 'products': products });
+		localStorage.setItem('products', obj);
+	});
+
 	
 	if($('body').hasClass('page-template-template-checkout')){
 		var url = 'http://localhost/terminsprojekt/hm/json/set_cart.php';
